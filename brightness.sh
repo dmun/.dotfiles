c7013cf9acd43f8682d9aafc4a782c4f1349e509
davidvanmunster <davidru01@live.nl>
2021-02-24 00:30:56 +0100

    working brightness.sh

--- brightness.sh
+++ brightness.sh
@@ -1,12 +1,14 @@
 #/bin/bash
 
-path=/sys/class/backlight/intel_backlight/brightness
-# path=test
+path=/sys/class/backlight/intel_backlight/
 
-brightness=$(cat $path)
+brightness=$(cat ${path}brightness)
+max_brightness=$(cat ${path}max_brightness)
 
-if [ $1 = "dec" ]; then
-    sed -i "s/${brightness}/$(( $brightness - 500 ))/g" $path
-elif [ $1 = "inc" ]; then
-    sed -i "s/${brightness}/$(( $brightness + 500 ))/g" $path
+if [ $1 = "--set" ]; then
+    echo $(($2 * $max_brightness / 100)) > ${path}brightness
+elif [ $1 = "--dec" ]; then
+    echo $(($brightness - $2)) > ${path}brightness
+elif [ $1 = "--inc" ]; then
+    echo $(($brightness + $2)) > ${path}brightness
 fi
