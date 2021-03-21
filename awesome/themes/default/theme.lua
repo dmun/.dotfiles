d3833139ab3fd7aaf4682c62bca2dee4f1cbff6d
davidvanmunster <davidru01@live.nl>
2021-03-21 20:40:21 +0100

    Theme changes

--- awesome/themes/default/theme.lua
+++ awesome/themes/default/theme.lua
@@ -11,7 +11,7 @@ local themes_path = "~/.config/awesome/themes/"
 
 local theme = {}
 
-theme.font          = "Product Sans 10"
+theme.font          = "Product Sans Medium 10"
 
 theme.bg_normal     = "#161616"
 theme.bg_focus      = "#202020"
@@ -103,7 +103,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path.."default/background.png"
+theme.wallpaper = themes_path.."default/clay-banks-u27Rrbs9Dwc-unsplash.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
