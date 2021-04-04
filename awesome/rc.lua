879e86b646f93f7a3276a6e9220165c5a9cf2b24
dmun <davidru01@live.nl>
2021-04-04 17:25:57 +0200

    Removed xrandr autostart

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -440,4 +440,3 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 -- Autostart
 awful.spawn.with_shell("xset r rate 200 60")
-awful.spawn.with_shell("xrandr --output HDMI-1 --same-as eDP-1")
