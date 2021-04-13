33f9f593467a6967bedc02c31c37eecc96f6f3ce
David van Munster <davidru01@live.nl>
2021-04-13 23:02:49 +0200

    Changes

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -197,31 +197,21 @@ end)
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
 client.connect_signal("request::titlebars", function(c)
     -- buttons for the titlebar
-    local buttons = gears.table.join(
-        awful.button({ }, 1, function()
-            c:emit_signal("request::activate", "titlebar", {raise = true})
-            awful.mouse.client.move(c)
-        end),
-        awful.button({ }, 3, function()
-            c:emit_signal("request::activate", "titlebar", {raise = true})
-            awful.mouse.client.resize(c)
-        end)
-    )
+    -- local buttons = gears.table.join(
+    --     awful.button({ }, 1, function()
+    --         c:emit_signal("request::activate", "titlebar", {raise = true})
+    --         awful.mouse.client.move(c)
+    --     end),
+    --     awful.button({ }, 3, function()
+    --         c:emit_signal("request::activate", "titlebar", {raise = true})
+    --         awful.mouse.client.resize(c)
+    --     end)
+    -- )
 
     positions = { "top", "right", "bottom", "left" }
 
     for i = 1, 4 do
         awful.titlebar(c, { position = positions[i], size = 2, bg_focus = "#3c3c3c", bg_normal = "#303030" }) : setup {
-            { -- Left
-                -- wibox.widget.imagebox(img, false, gears.shape.rounded_rect),
-                layout  = wibox.layout.fixed.horizontal
-            },
-            { -- Middle
-                layout  = wibox.layout.flex.horizontal
-            },
-            { -- Right
-                layout = wibox.layout.fixed.horizontal()
-            },
             layout = wibox.layout.align.horizontal
         }
     end
