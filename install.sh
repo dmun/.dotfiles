72d77e923534ded2de4aca9d5e946fb582b4f7c3
David van Munster <davidru01@live.nl>
2021-05-04 20:25:43 +0200

    Add zathura

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ script_dir="$(dirname $(readlink -f $0))"
 conf_dir=$HOME/.config
 
 # linked files
-files="bspwm sxhkd polybar dunst fontconfig alacritty nvim rofi picom"
+files="bspwm sxhkd polybar dunst fontconfig alacritty nvim rofi zathura picom"
 dotfiles=".zshrc .tmux.conf .Xmodmap"
 
 # create config folder if it doesn't exist
