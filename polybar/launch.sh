ffafe91e06b24c505452d0fab3e66c998800b2ad
dmun <davidru01@live.nl>
2022-03-16 00:32:42 +0100

    Add dpi support polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,5 +6,12 @@ killall -q polybar
 ## Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
+## Handle DPI
+dpi=$(xrdb -query | grep -oP "Xft.dpi:\s*\K\d*")
+if [ -z $dpi ]; then dpi=96; fi
+scaling=$(expr $dpi \* 100 / 96)
+height=$(expr 30 \* $scaling / 100)
+echo "polybar.height: $height" | xrdb -merge
+
 ## Launch
 polybar main -c ~/.config/polybar/config.ini &
