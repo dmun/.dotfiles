f2db358440b6b59bbbc4ab24dc2d47b4dc33ff9f
dmun <davidru01@live.nl>
2024-04-18 20:24:12 +0200

    hammerspoon: add ctrl-j escape

--- .hammerspoon/hotkeys.lua
+++ .hammerspoon/hotkeys.lua
@@ -4,6 +4,10 @@ function M:init()
 	self.reload = hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", function()
 		hs.reload()
 	end)
+
+	self.reload = hs.hotkey.bind({ "ctrl" }, "j", function()
+		hs.eventtap.keyStroke({}, "escape", 0)
+	end)
 end
 
 return M
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,2 @@
-_G.ctrl_esc = require("ctrl_esc")
-_G.ctrl_esc:init()
-
 _G.hotkeys = require("hotkeys")
 _G.hotkeys:init()
