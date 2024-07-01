6d3cf639ef2ade9c688325c28b6f41d56129e7e8
dmun <davidru01@live.nl>
2024-07-01 17:29:15 +0200

    f

--- fish/config.fish
+++ fish/config.fish
@@ -29,4 +29,5 @@ end
 set -x ESCDELAY 1
 set -x EDITOR nvim
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden'
-set -x FZF_DEFAULT_OPTS '--color=bg+:#39424D'
+# set -x FZF_DEFAULT_OPTS '--color=bg+:#39424D'
+set -x FZF_DEFAULT_OPTS '--no-separator'
