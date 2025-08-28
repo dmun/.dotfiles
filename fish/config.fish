82d3916318054a1d557263f39bd315c10ff198e8
David van Munster <davidru01@live.nl>
2025-08-28 02:28:44 +0200

    f

--- fish/config.fish
+++ fish/config.fish
@@ -25,3 +25,25 @@ set -x EDITOR nvim
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden'
 # set -x FZF_DEFAULT_OPTS '--color=bg+:#39424D'
 set -x FZF_DEFAULT_OPTS '--no-separator'
+
+set -x FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS'
+  --style=full:sharp
+  --no-border
+  --no-header-border
+  --no-input-border
+  --no-list-border
+  --color=fg:-1,bg:-1,fg+:0,bg+:7
+  --color=hl:10,hl+:blue,info:8,marker:2
+  --color=prompt:7,spinner:6,pointer:#af5fff,header:#87afaf
+  --color=gutter:-1,border:8,label:#aeaeae,query:#d9d9d9
+  --color=info:8
+  --marker="▌"
+  --prompt="/"
+  --pointer=""
+  --separator=""
+  --scrollbar=""
+  --height=40%
+  --layout=reverse'
+set -x _ZO_FZF_OPTS $FZF_DEFAULT_OPTS
+
+set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
